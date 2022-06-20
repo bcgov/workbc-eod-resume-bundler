9692e7679525ee32017ea5f1d2b0d1e546c2747f
Branko Bajic <branko@mailfence.com>
2022-06-20 14:55:30 -0700

    Fix dropzone bug and bug where resume buffer wasnt being correctly sent to the api

--- client/src/utils/FileFunctions.js
+++ client/src/utils/FileFunctions.js
@@ -1,3 +1,5 @@
+import {Buffer} from 'buffer';
+
 export const b64toBlob = (b64Data, contentType='', sliceSize=512) => {
     const buffer = Buffer.from(b64Data, "base64");
     const blob = new Blob([buffer.buffer], {type: contentType});
